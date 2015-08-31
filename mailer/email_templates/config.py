49f927375cb284cfee7c39931bf1c38c54c0df8b
Oskar Hollmann <oskar@hollmann.me>
2015-08-31 18:54:42 +0200

    Template sender uses template model from settings if available

--- mailer/email_templates/config.py
+++ mailer/email_templates/config.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
-
 try:
     EMAIL_DEFAULT_FROM_EMAIL = settings.EMAIL_DEFAULT_FROM_EMAIL
 except:
@@ -13,3 +12,12 @@ try:
     EMAIL_DEFAULT_PRIORITY = settings.EMAIL_DEFAULT_PRIORITY
 except:
     raise ImproperlyConfigured('email_templates needs EMAIL_DEFAULT_PRIORITY set in settings.py')
+
+try:
+    if hasattr(settings, 'MAILER_TEMPLATE_MODEL'):
+        app_name, class_name = settings.MAILER_TEMPLATE_MODEL.split('.')
+        MAILER_TEMPLATE_MODEL = (app_name, class_name)
+    else:
+        MAILER_TEMPLATE_MODEL = None
+except:
+    raise ImproperlyConfigured('MAILER_TEMPLATE_MODEL in settings.py is not valid. Should be "app_name.class_name".')
